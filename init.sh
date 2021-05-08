7d96cd7c13bafbc3e91492c28cf0a4f77fe7acb7
Marc Anton Dahmen <mail@marcdahmen.de>
2021-05-08 17:36:50 +0200

    Added self update on first run

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 if [ ! -f config/accounts.php ]; then
+	php automad/console update
 	php automad/console createuser
+	rm -rf cache/update
 fi
 
 /usr/bin/supervisord -c /etc/supervisord.conf
